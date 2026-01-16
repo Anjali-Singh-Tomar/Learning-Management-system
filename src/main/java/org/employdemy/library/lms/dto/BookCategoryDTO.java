868ac7c8464a8495d369d71555f7feb56637d850
Anjali-Singh-Tomar <tomaranjalisingh2812@gmail.com>
2026-01-16 12:51:50 +0530

    modified the books by category

--- src/main/java/org/employdemy/library/lms/dto/BookCategoryDTO.java
+++ src/main/java/org/employdemy/library/lms/dto/BookCategoryDTO.java
@@ -2,11 +2,12 @@ package org.employdemy.library.lms.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.employdemy.library.lms.model.Genre;
 
 @Data
 @AllArgsConstructor
 public class BookCategoryDTO {
 
-    private String genre;
-    private int count;
+    private Genre genre;
+    private Long count;
 }
