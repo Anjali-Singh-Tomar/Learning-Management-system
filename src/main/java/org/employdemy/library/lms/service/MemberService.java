8d7a236d6d74eb77b2c0073e7bd025f6e875fb36
Srajput1506 <8as1931091@gmail.com>
2026-01-16 13:23:01 +0530

    Again fixed some bugs on members API

--- src/main/java/org/employdemy/library/lms/service/MemberService.java
+++ src/main/java/org/employdemy/library/lms/service/MemberService.java
@@ -42,8 +42,8 @@ public class MemberService {
 
         return new MemberDashboardResponseDTO(
                 currentlyBorrowed,
-                booksRead,
-                dueSoon
+                dueSoon,
+                booksRead
         );
     }
 
