01f34dd8247686e6d377887a779ce0687a98b264
Anjali-Singh-Tomar <tomaranjalisingh2812@gmail.com>
2026-01-19 12:36:23 +0530

    forgot pwd .

--- src/main/java/org/employdemy/library/lms/controller/AuthController.java
+++ src/main/java/org/employdemy/library/lms/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.employdemy.library.lms.service.OtpService;
 import org.employdemy.library.lms.service.OtpStore;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
